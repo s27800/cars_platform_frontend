63942d07b50abaed2726c5d30478e57e6512cc8d
s27800 <s27800@pjwstk.edu.pl>
2026-06-03 19:39:03 +0200

    Feat: - Created Profile Page - Added prrofile info, profile edit and password change functionalities

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,3 +5,4 @@ export { default as RegisterPage } from './RegisterPage';
 export { default as CarsSearchPage } from './CarsSearchPage';
 export { default as CarDetailsPage } from './CarDetailsPage';
 export { default as ComparisonPage } from './ComparisonPage';
+export { default as ProfilePage } from './ProfilePage';
